7eeb702a3b685a24f00c78be70fbe6ab71eddd45
Ivan Remizov <iremizov@gmail.com>
2017-11-13 20:43:51 +0300

    Refactor UsersManager

--- src/users-manager.js
+++ src/users-manager.js
@@ -1,4 +1,4 @@
-const { diffLists } = require("./util");
+const { diffIgnoreableObjects } = require("./util");
 
 class UsersManager {
   constructor(app) {
@@ -22,13 +22,9 @@ class UsersManager {
     return this.app._decrypt;
   }
 
-  async fetch() {
-    this.remoteData.users = this.remoteData.users || {};
-
-    // Get users and their details
-    (await this.client.get("admin/users", {
-      query: { limit: 1000 }
-    })).values.forEach(({ name, emailAddress, displayName, slug }) => {
+  async fetchUsers() {
+    const data = await this.client.getAll("admin/users");
+    data.values.forEach(({ name, emailAddress, displayName, slug }) => {
       if (slug !== name) {
         // FIXME console.warn
         // eslint-disable-next-line no-console
@@ -39,93 +35,105 @@ class UsersManager {
         email: emailAddress
       };
     });
+  }
 
-    // Now it's possible to find out user global permission (role)
-    (await this.client.get("admin/permissions/users", {
-      query: { limit: 1000 }
-    })).values.forEach(
+  async fetchUsersPermissions() {
+    const data = await this.client.getAll("admin/permissions/users");
+    data.values.forEach(
       ({ user: { slug }, permission }) =>
         (this.remoteData.users[slug].permission = permission)
     );
   }
+
+  async fetch() {
+    this.remoteData.users = this.remoteData.users || {};
+    await this.fetchUsers();
+    await this.fetchUsersPermissions();
+  }
+
+  async createUser(data) {
+    await this.client.post("admin/users", {
+      query: {
+        addToDefaultGroup: false,
+        notify: false,
+        ...data
+      }
+    });
+  }
+
+  async updateUser(name, data) {
+    await this.client.put("admin/users", {
+      data: {
+        name,
+        ...data
+      }
+    });
+  }
+
+  async removeUser(name) {
+    await this.client.delete("admin/users", {
+      query: { name }
+    });
+  }
+
+  async setUserPermission(name, permission) {
+    await this.client.put("admin/permissions/users", {
+      query: { name, permission }
+    });
+  }
+
+  async removeUserPermission(name) {
+    await this.client.delete("admin/permissions/users", { query: { name } });
+  }
+
   async apply() {
-    const toIgnore = Object.keys(this.localData.users).filter(
-      slug => this.localData.users[slug] === "ignore"
-    );
-    const [toAdd, toChange, toRemove] = diffLists(
-      Object.keys(this.localData.users).filter(
-        slug => !toIgnore.includes(slug)
-      ),
-      Object.keys(this.remoteData.users).filter(
-        slug => !toIgnore.includes(slug)
-      )
+    const [toAdd, toChange, toRemove] = diffIgnoreableObjects(
+      this.localData.users,
+      this.remoteData.users
     );
 
-    for (const userSlug of toAdd) {
-      const localUser = this.localData.users[userSlug];
-      await this.client.post("admin/users", {
-        query: {
-          name: userSlug,
-          displayName: localUser.displayName,
-          emailAddress: localUser.email,
-          password: await this._decrypt(localUser.password),
-          addToDefaultGroup: false,
-          notify: false
-        }
+    for (const name of toAdd) {
+      const { email, password, displayName, permission } = this.localData.users[
+        name
+      ];
+      await this.createUser({
+        name,
+        emailAddress: email,
+        displayName,
+        password: await this._decrypt(password)
       });
-      if (localUser.permission) {
-        // create permission
-        await this.client.put("admin/permissions/users", {
-          query: { permission: localUser.permission, name: userSlug }
-        });
+      if (permission) {
+        await this.setUserPermission(name, permission);
       }
     }
 
-    for (const userSlug of toRemove) {
-      const remoteUser = this.remoteData.users[userSlug];
-      if (remoteUser.permission) {
-        // remove permissions
-        await this.client.delete("admin/permissions/users", {
-          query: { name: userSlug }
-        });
+    for (const name of toRemove) {
+      const { permission } = this.remoteData.users[name];
+      if (permission) {
+        await this.removeUserPermission(name);
       }
-      await this.client.delete("admin/users", {
-        query: { name: userSlug }
-      });
+      await this.removeUser(name);
     }
 
-    for (const userSlug of toChange) {
-      const localUser = this.localData.users[userSlug];
-      const remoteUser = this.remoteData.users[userSlug];
+    for (const name of toChange) {
+      const { displayName, email, permission } = this.localData.users[name];
+      const remoteUser = this.remoteData.users[name];
       if (
-        localUser.email !== remoteUser.email ||
-        localUser.displayName !== remoteUser.displayName
+        email !== remoteUser.email ||
+        displayName !== remoteUser.displayName
       ) {
-        // sync properties
-        await this.client.put("admin/users", {
-          data: {
-            name: userSlug,
-            displayName: localUser.displayName,
-            email: localUser.email
-          }
+        await this.updateUser(name, {
+          displayName,
+          email
         });
       }
 
-      if (!localUser.permission && remoteUser.permission) {
-        // remove permission
-        await this.client.delete("admin/permissions/users", {
-          query: { name: userSlug }
-        });
+      if (!permission && remoteUser.permission) {
+        await this.removeUserPermission(name);
       }
 
-      if (
-        localUser.permission &&
-        localUser.permission !== remoteUser.permission
-      ) {
-        // change permission
-        await this.client.put("admin/permissions/users", {
-          query: { permission: localUser.permission, name: userSlug }
-        });
+      if (permission && permission !== remoteUser.permission) {
+        await this.setUserPermission(name, permission);
       }
     }
   }
